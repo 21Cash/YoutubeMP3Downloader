a3e48b92a71d51d9593be2e3a11b8830d115345c
21ThousandProof <21ThousandProof.@gmail.com>
2023-01-21 10:40:27 +0530

    Refactored Main Code

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,32 +1,42 @@
 from pytube import YouTube
 
 
+
+def DownloadMP3(s):
+	yt = YouTube(s)
+
+
+	print("Title : " + yt.title)
+
+
+	print("Fetching the all Audio Files", flush = True)
+	streams = yt.streams.filter(only_audio=True)
+
+	for s in streams:
+		print(s)
+
+	print("Getting The Stream", flush = True)
+
+	stream = yt.streams.last()
+
+	print("itag = " + str(stream.itag))
+
+	print("Download Started", flush = True)
+
+	stream.download()
+
+	print("Download Done.")
+
+
+# Main Code
+
 inputFile = open("input.txt", "r")
 logFile = open("log.txt", "w")
+DownloadMP3(inputFile.readline())
 
 
 
-yt = YouTube(inputFile.readline())
 
 
-print("Title : " + yt.title)
 
-
-print("Fetching the all Audio Files", flush = True)
-streams = yt.streams.filter(only_audio=True)
-
-for s in streams:
-	print(s)
-
-print("Getting The Stream", flush = True)
-
-stream = yt.streams.last()
-
-print("itag = " + str(stream.itag))
-
-print("Download Started", flush = True)
-
-stream.download()
-
-print("Download Done.")
 
