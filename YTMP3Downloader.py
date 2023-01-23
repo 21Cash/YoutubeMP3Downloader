15038c1a4b71a322838bc2274d241b17eec2d7c8
21ThousandProof <21ThousandProof.@gmail.com>
2023-01-23 17:06:48 +0530

    Refactored YTMP3

--- YTMP3Downloader.py
+++ YTMP3Downloader.py
@@ -1,53 +1,93 @@
+import time
+import datetime
 from pytube import YouTube
 
+class Timer:
+	def __init__(self):
+		self.start = 0
+		self.end = -1
+	def Start(self):
+		self.start = time.time();
+	def Stop(self):
+		end = time.time();
+		return (end - self.start);
+
+
+
+
+def getFormattedTime(curTime):
+	_str = f"{curTime.day} / {curTime.month} / {curTime.year}, [{curTime.hour} : {curTime.minute} : {curTime.second}]" 
+	return _str 
+
+def getCurrentTime():
+	return getFormattedTime(datetime.datetime.now())
+
+def getMs(timeInSeconds):
+	return int(timeInSeconds * 1000)
+
+
 def log(str):
-	logFile.write(str)
+	print(str, flush = True)
+	logFile.write(getCurrentTime() + "  :  " + str)
 	logFile.write("\n")
 	logFile.flush();
+ 
 
 def DownloadMP3(s):
+	
 	yt = YouTube(s)
-
-
-	print("Title : " + yt.title)
 	log(f"Title : {yt.title}")
 
-	print("Fetching the all Audio Files", flush = True)
-	log(f"Getting The Audio Stream")
+	log(f"Fetching The Audio Streams")
+	
 	streams = yt.streams.filter(only_audio=True)
 	
-	log("Streams Found")
-	log("STREAMS: ")
+	log("\n");
+	log("Streams: ")
 	for s in streams:
 		log(str(s))
-		print(s)
-
-	print("Getting The Stream", flush = True)
+	log("\n")
 
 	stream = yt.streams.last()
-
-	print("itag = " + str(stream.itag))
+	
+	log("Downloading Stream with itag = " + str(stream.itag))
+	
+	log(f"FileSize : {round((stream.filesize) / (1024 * 1024), 2)} MB")
 	
 	log(f"Download Started, itag = {stream.itag}")
 	
-	print("Download Started", flush = True)
 
 	stream.download()
 
-	print("Download Done.")
 	log("Download Completed.")
 
 
-# Main Code
+def mainCode():
+		# Main Code
+	timer = Timer()
+	timer.Start()
+
+
+	
+	DownloadMP3(inputFile.readline())
+
+	t = timer.Stop()
+
+	log(f"Execution Time : {round(t, 2)} second ({getMs(t)} ms)")
+
+	log("Program Terminated Safely.")
+	input()
+
+
+
 
 inputFile = open("link.txt", "r")
 logFile = open("log.txt", "w")
-DownloadMP3(inputFile.readline())
 
-log("Program Terminated Safely.")
-
-
-
+try:
+	mainCode()
+except Exception as Argument:
+	log(str(Argument))
 
 
 
